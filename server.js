907a8497aee831354442e902091d6d3bcecbeb61
Shubham <Shubham@gmail.com>
2024-05-21 00:23:47 +0530

    first

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 import http from "http";
-import app from "./app/app";
+import app from "./app/app.js";
 
+// Create the server
+const PORT = process.env.PORT || 2030;
 const server = http.createServer(app);
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
