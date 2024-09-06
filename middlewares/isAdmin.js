703fda534a1e69710a7456d2c60a11ad8ccf4e79
Shubham Kumar <johndoe@email.com>
2024-09-06 22:50:58 +0530

    commit changes

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -3,7 +3,7 @@ import User from "../model/User.js";
 export const isAdmin = async (req, res, next) => {
 	//! find the login user
 	const user = await User.findById(req.user.id);
-	//! Check user is Amin
+	//! Check user is Admin
 	if (!user.isAdmin) {
 		return res.status(401).json({
 			status: "Failed",
